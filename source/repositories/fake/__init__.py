c427368e14d9eacc18c4c9910d9edaa68aa57e11
Thibaut Pillon <thibautpillon@MacBook-Pro-de-Thomas.local>
2026-05-05 09:55:41 +0200

    clean

--- source/repositories/fake/__init__.py
+++ source/repositories/fake/__init__.py
@@ -1,4 +1,4 @@
 from .tasks_fake_repository import TasksFakeRepository
 from .users_fake_repository import UsersFakeRepository
 
-__all__ = [TasksFakeRepository, UsersFakeRepository]
+__all__ = ["TasksFakeRepository", "UsersFakeRepository"]
